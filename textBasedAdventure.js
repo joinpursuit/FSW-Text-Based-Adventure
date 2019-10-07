2531ede544076ccc69570c3a051595fcd2362de7
Kevin Wong <kevinwong@pursuit.org>
2019-10-07 16:10:06 -0400

    Revise Switch statement

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -8,28 +8,32 @@ let portal = readline.question("Please pick a Portal 1-3: ");
 
 const area1 = "Coliseum";
 const area2 = "Mars";
-const area3 = "";
+const area3 = "President Trump's Office";
     
+
     switch(portal){
 
         case '1': 
             console.log("You have teleported to " + area1);
-            let x1 = readline.question("Welcome to the ARENA. You are fighting against another gladiator." + "\n" +
+            let a1 = readline.question("Welcome to the ARENA. You are fighting against another gladiator." + "\n" +
             "You can either 1- dodge, 2- defend, or 3 - attack: ");
-            if (x1 === "1" || x1.toLowerCase() === 'dodge' ){
+            if (a1 === "1" || a1.toLowerCase() === 'dodge' ){
                 console.log("You try to dodge the incoming gladiator's attack, but tripped on a rock and he impales you with his blade. GAME OVER!");
-                break;
-            } else if (x1 === "2" || x1.toLowerCase() === 'defend'){
+            } else if (a1 === "2" || a1.toLowerCase() === 'defend'){
                 console.log("You try to defend the attack. The opponent's blade is far too sharp and cracks yours' into two. Being at a major disadvantage - he ends up impaling you with his blade. GAME OVER!");
-                break;
-            } else if (x1 === '3' || x1.toLowerCase() === 'attack'){
+            } else if (a1 === '3' || a1.toLowerCase() === 'attack'){
                 console.log("Moving with swiftness, you lunge forward towards the gladiator. Startled, he swings his blade with inaccuracy and misses his counterattack. You impale him while his guard is low." + "\n" + "The battle is won!");
+                
+                let p1 = readline.question("You are now crowned the champion of the Coliseum. You are selected to choose a prize: 100000 gold, Champion sword, or Shield: ");
+                while (p1){
+                    if (p1)
+                }
+                
                 break;
             } else {
-                console.log("Please choose an action 1-3 or perish")
-                jump : 16
+                console.log("Please choose an action 1-3 or perish");
             }
-        break;
+            break;
 
         case '2':
             console.log("You have teleported to " + area2);
