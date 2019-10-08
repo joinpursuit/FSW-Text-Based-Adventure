354a5daade77f83ec1e7ffb7ed959d579fb57479
syn perez <synperez@putsuit.org>
2019-10-07 21:05:39 -0400

    rough draft 1

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,5 +1,14 @@
 const readline = require('readline-sync')
 
 let nameInput = readline.question("Enter your name: ")
+let name = nameInput
 
 console.log(`Hello ${nameInput}!  Welcome to my game.`)
+console.log(readline.question(name + ", are you over the age of 18? Please answer yes or no: "))
+if ("yes"|| "Yes"|| "yEs"|| "yeS"|| "yES"|| "YeS"|| "YES"){
+    console.log('Welcome ' + name)
+} else if ("no" || "No" || "nO") {
+    console.log("come back when you are 18," + name)
+} else {
+    console.log("Please answer yes or no")
+}
