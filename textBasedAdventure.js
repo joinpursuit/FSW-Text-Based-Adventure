d6b6ba4c0ef2f2c11ce5c36bdd682e182ad822c4
trilltown <phil.awich@gmail.com>
2019-10-07 21:30:48 -0400

    Changes to Text Game

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,5 +1,18 @@
+
 const readline = require('readline-sync')
 
-let nameInput = readline.question("Enter your name: ")
+let intro = ("Welcome to 'Soul Searcher'");
+console.log(intro)
+let dialogue = ("In this adventure you will be embark on a quest to find your missing soul mate");
+console.log(dialogue)
 
-console.log(`Hello ${nameInput}!  Welcome to my game.`)
+let enterGame = readline.question("Press 'enter' to continue")
+
+let nameInput = readline.question("Who are you?")
+let hello = readline.question(`Hi ${nameInput}`)
+let Number = readline.question("What's your favorite number?")
+console.log(typeof Number)
+
+
+
+// console.log(`Hello ${nameInput}!  Welcome to my Soul Searcher.`)
