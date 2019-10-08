b2b66baec53c665303ba7858206e7ad8aae7110e
Marvin Thompson <marvinthompson@pursuit.org>
2019-10-07 21:27:31 -0400

    adding options

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -2,26 +2,41 @@ const readline = require('readline-sync')
 let titleCard = readline.question("CAVE HERO!")
 console.log(`${titleCard}`)
 let nameInput = readline.question("Enter your name: ")
-let overviewStatement = "We need your help. Our princess has been captured, and we need you to rescue her. Can we count on you?"
 console.log(`Greetings ${nameInput}!`)
+
 let caveEntrance = "You arrive at the entrance of a dimly lit cave aligned with torches. From the torchlight you see there are three tunnel entrances: One to the left, one to the right, and one to the center. Which tunnel will you enter?"
+let overviewStatement = "We need your help. Our princess has been captured, and we need you to rescue her. Can we count on you?"
+let missionStart = readline.question(`${overviewStatement} Type yes or no.`)
+let battleStart = "You encountered an enemy! What will you do?: "
+battleStart = battleStart.toLowerCase()
+missionStart = missionStart.toLowerCase()
+
+                if (missionStart === "yes") {
+                    console.log(caveEntrance)
+                } else {     
+                    console.log("Your cowardice shames us - Good day!")
+                }
+
+
 let tunnels = readline.question("Enter LEFT, CENTER, RIGHT: ")
 tunnels = tunnels.toLowerCase()
-let tunnelOutcomesArr = ["get attacked","You rescued her!","Nothing here. Go back?"]
-let itemDropArr = ["SWORD", "SHIELD", "MEAT", "DOOR KEY",]
+let tunnelOutcomesArr = [readline.question(battleStart),"You rescued her!","Nothing here. Go back?"]
 let randomOutcome = Math.floor(Math.random() * 10)
-let missionStart = readline.question(`${overviewStatement} Type yes or no.`)
-missionStart = missionStart.toLowerCase()
-//
 
+// ENEMY VARIABLES
+
+
+//ITEM VARIABLES 
+let sword = 1
+let shield = 2
+let meat = 3
+let torch = 4
+let monsterSword = 5
+let doorKey = 6
+let itemDropArr = [sword, shield, meat, torch, monsterSword, doorKey]
 
 // 
 
-if (missionStart === "yes") {
-    console.log(caveEntrance)
-} else {     
-    console.log("Your cowardice shames us - Good day!")
-}
     
 // let tunnelChoice = []
 
