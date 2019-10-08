e6bba4f37df37bce2fdce0ca80d55211ef3c3be4
kongnyc <54258089+kongnyc@users.noreply.github.com>
2019-10-08 01:21:42 -0400

    loops finished but doesnt work

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -3,20 +3,27 @@ const readline = require('readline-sync')
 let nameInput = readline.question("Enter your name: ")
 console.log(`Hello ${nameInput}!  Welcome to my Island.`)
 
-let monK1 = readline.question("Enter an animal name")
-console.log(monK1)
-console.log("The Game Began, you are in vacation island and you ship is waiting for you")
-console.log(`${monK1}! who stolen your wallet and you want to get that back. your ticket of ship is inside that Wallet`)
-console.log(`smarty pants ${monK1}! is in top of Plam tree, Please knok it down to retrieve you Wallet`)
-console.log("Then I start look around and I find some weapons over the island")
-var readlineSync = require('readline-sync'),
-  Weapons = ['Rock1', 'Rock2', 'Rock3', 'Rock4', 'Rock5'],
-  index = readlineSync.keyInSelect(Weapons, 'Which One?');
-console.log("I Pick up, " + Weapons[index] + `" throw it to ${monK1}"`);
+let monsK1 = readline.question("Enter an animal name: ")
+let monsK1LifeHP = 100
+console.log("\nYou are in vacation island and you ship is waiting for you")
+console.log(`\n${monsK1}! who stolen your wallet, your ticket of the ship is inside`)
+console.log(`\nSmarty pants ${monsK1}! is in top of Plam tree, \nPlease knok it down to retrieve you Wallet`)
+console.log(`\n\n\nThen ${nameInput} start look around and I find some weapons over the island
+`)
 
+
+while (monsK1LifeHP <= 100){
+if(monsK1LifeHP > 0){
+var readlineSync = require('readline-sync');
+Weapons = ['Useless Rock', 'Empty Bottle', 'Blue Craby', 'Dead Fish', 'Gaint Rock'],
+console.log("\n\n\n\n\n```Select from Weapons List below```")
+index = readlineSync.keyInSelect(Weapons, 'Pick your weapons?');
+console.log("\n\n\n\n\nI Pick up a weapon [" + Weapons[index] + `] throw it to ${monsK1}.`);
+
+//wepower selection
 var readlineSync = require('readline-sync'),
-MAX = 10, MIN = 0, value = 10, key;
-console.log(`How hard you want to throw that${monK1}` )
+MAX = 10, MIN = 0, value = 5, key;
+console.log(`\nHow hard you want to throw that to ${monsK1}?` )
 console.log('\n\n' + (new Array(2)).join(' ') +
   '[Z] <- -> [X]  Enter: [SPACE]\n');
 while (true) {
@@ -29,4 +36,15 @@ while (true) {
   else if (key === 'x') { if (value < MAX) { value++; } }
   else { break; }
 }
+value = value * Math.floor(Math.random() * 10)//random att from 1-10
 console.log(`\n${nameInput} try hit with power: ` + value);
+monsK1LifeHP -= value;
+console.log(`\n${monsK1} taking damage ${monsK1LifeHP}`);
+console.log(monsK1LifeHP);
+} 
+else (monsK1LifeHP < 0); {
+console.log(`${nameInput} knok down ${monsK1} badly, \nCongratulations, retrieve your ticket, \nYou can go Home `)
+console.log(monsK1LifeHP);
+break;
+}
+}
