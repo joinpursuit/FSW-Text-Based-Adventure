f076b8d808f02eeeee1123b2545ea4c7a1219e50
syn perez <synperez@putsuit.org>
2019-10-08 01:46:26 -0400

    just a save

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,14 +1,19 @@
 const readline = require('readline-sync')
 
-let nameInput = readline.question("Enter your name: ")
+console.log("                       Street Explorer: The journey that no one wants to go on                       ")
+
+
+let nameInput = readline.question("Please enter your name: ")
+
 let name = nameInput
 
-console.log(`Hello ${nameInput}!  Welcome to my game.`)
+console.log(`What's up ${nameInput}!  Welcome to my game.`)
 console.log(readline.question(name + ", are you over the age of 18? Please answer yes or no: "))
 if ("yes"|| "Yes"|| "yEs"|| "yeS"|| "yES"|| "YeS"|| "YES"){
-    console.log('Welcome ' + name)
+    console.log("you've received your Hood Pass " + name)
 } else if ("no" || "No" || "nO") {
     console.log("come back when you are 18," + name)
 } else {
     console.log("Please answer yes or no")
 }
+
