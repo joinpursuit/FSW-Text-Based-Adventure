6337df7718fda855d44c4a13ace38e377c879390
Kevin Brutus <kevinbrutus@pursuit.org>
2019-10-07 21:27:04 -0400

    Condense lines

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -19,18 +19,26 @@ if(ageInput <= 19){
 }
 
 // Team choice based on color preference
+let teamLight = 'Your weapon is a sword. Your protection is armor.'
+let teamDark = 'Your weapon is a spear. Your protection is a shield.'
+
 let colorInput = readline.question('What color would you like (please only use one): ')
-    if (colorInput === 'blue' || colorInput=== 'red' || colorInput === 'orange' || colorInput === 'yellow' || colorInput === 'white' || colorInput === 'green' || colorInput === 'pink'){
-        console.log('You are Team Light')
-        let teamLight = 'Your weapon is a sword. Your protection is armor.'
-        console.log(teamLight)
-    } else if (colorInput === 'purple' || colorInput === 'black' || colorInput === 'grey' || colorInput === 'brown' || colorInput === 'gray'){
-        console.log('You Team Dark')
-        let teamDark = 'Your weapon is a spear. Your protection is a shield.'
-        console.log(teamDark)
-    } else {
+if (colorInput === 'blue' || colorInput=== 'red' || colorInput === 'orange' || colorInput === 'yellow' || colorInput === 'white' || colorInput === 'green' || colorInput === 'pink'){
+    console.log('You are Team Light' + teamLight)
+} else if (colorInput === 'purple' || colorInput === 'black' || colorInput === 'grey' || colorInput === 'brown' || colorInput === 'gray'){
+    console.log('You Team Dark' + teamDark)
+}else {
         console.log('Your color taste it too exquisite for our game. Please try again.')
         readline.question('What color would you like (please only use one): ')
     }    
 
+// let team1 = teamLight
+// let team2 = teamDark
 
+// if (team2){
+//     console.log('Welcome to the Darkness. You are more powerful than our light adversary. To war we go!')
+//     readline.question('What would your first act of war be: ')
+// } else if(team1){
+//     console.log('Welcome to the Light. We must defeat the darkness once and for all to keep balance between the realms')
+//     readline.question('What would your first act of peace be: ')
+// }
