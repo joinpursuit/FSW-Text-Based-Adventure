49b396d8f9763785d537eabf687d293830cd1184
Cassidy Beni <cassidybeni@pursuit.org>
2019-10-09 21:13:33 -0400

    Updated

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -6,23 +6,28 @@ console.log(`Hello ${nameInput}!  Welcome to my game.`)
 
 //Open the box
 let readlineSync = require('readline-sync');
-if(readlineSync.keyInYN("You are at a park and find a box with your name on it. Would you like to open the box? (Y/N)")) {
-    //'Y' key was pressed
- console.log("There is $10,000 in the box!");
-} else {
-    //N was pressed
- console.log("You missed out on $10,000 :( ")
+let i = readlineSync.keyInYN("You are at a park and find a box with your name on it. Would you like to open the box? (Y/N)")
+
+while(i !== 'y' && i === 'n'){
+  i = readlineSync.keyInYN("You are at a park and find a box with your name on it. Would you like to open the box? (Y/N)")
 }
+  if(readlineSync.keyInYN("You are at a park and find a box with your name on it. Would you like to open the box? (Y/N)")) {
+      //'Y' key was pressed
+   console.log("There is $10,000 in the box!");
+  } else {
+      //'N' was pressed
+   console.log("You missed out on $10,000. (Answer yes) ")
+  }
+
 
 
 //How will you spend it?
 let readlineSync1 = require('readline-sync'),
-  decisions = ['quit my job', 'invest', 'travel'],
+  decisions = ['pay off student loans', 'buy a new car', 'travel for a year'],
   index = readlineSync1.keyInSelect(decisions, 'Which option?');
 console.log('Ok,' + " you chose " + decisions[index]);
 
 //
-
 
 
 
