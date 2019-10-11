7c604783085cf09ec57d8d78429b1b93d495590a
david maravillas <davidmaravillas@pursuit.org>
2019-10-10 20:48:59 -0400

    player stats set

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,44 +1,57 @@
 const readline = require('readline-sync')
 
-let race = [ "Human", "Elf","Orc"]
+let race = [ "human", "elf","orc"]
 
 let weapon = ["Sword","Axe","Spear"]
 
-let raceStat = {
-    Human:[10,10],
-    Elf:[12,8],
-    Orc:[8,12]
-}
 let weaponStat = {
     Sword:0,
     Axe:0,
     Spear:0
 }
+let playerHealth = 0;
+let playerAttack = 0;
+let armedPlayer = 0;
 
 let nameInput = readline.question("Enter your name: ")
 
 console.log(`Hello ${nameInput}!  Welcome to your first adventure.`)
 
 let raceInput = readline.question(`Choose your race ${race}.`)
-let chosenRace = raceInput
-console.log(raceInput)
 
+let raceStat = []
 
-raceInputError = chosenRace
-do{ let raceInputError = readline.question(`Sorry, that's not a correct race, Choose your race ${race}.`);
-}
-while(chosenRace !== "human" || chosenRace !== "elf" || chosenRace !== "orc");
-
-
-
-let beginStat = ""
-
-if(chosenRace === "human"){
-    beginStat = raceStat["Human"]
-} else if (chosenRace === "elf"){
-    beginStat = raceStat["Elf"]
-} else if (chosenRace === raceStat["orc"]){
-    beginStat = raceStat["Orc"]
+if (raceInput = "human"){
+    raceStat = [10,10];
+} else if (raceInput = "elf"){
+    raceStat = [12,8]
+} else if (raceInput = "orc"){
+    raceStat = [8,12];
 }
 
-console.log(`your starting stats are ${Number(beginStat)}`)
+
+playerHealth = raceStat[0]
+playerAttack = raceStat[1]
+
+console.log(`your race is ${raceInput} health ${playerHealth} attack ${playerAttack}`)
+
+
+// do{ let raceInputError = readline.question(`Sorry, that's not a correct race, Choose your race ${race}.`);
+// if(raceInputError === "human" || raceInputError === "elf" || raceInputError === "orc"){break;}
+// }
+// while(raceInputError !== "human" || raceInputError !== "elf" || raceInputError !== "orc");
+// raceInputError.tolowercase;
+
+
+
+// let beginStat = ""
+
+// if(chosenRace === "human"){
+//     beginStat = raceStat["Human"]
+// } else if (chosenRace === "elf"){
+//     beginStat = raceStat["Elf"]
+// } else if (chosenRace === raceStat["orc"]){
+//     beginStat = raceStat["Orc"]
+// }
+
+// console.log(`your starting stats are ${Number(beginStat)}`)
