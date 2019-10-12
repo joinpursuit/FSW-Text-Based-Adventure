b99a4568e028785694d1492bbd2dddd496b13e0e
kongnyc <54258089+kongnyc@users.noreply.github.com>
2019-10-12 14:01:52 -0400

    loop finished

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -4,18 +4,19 @@ let nameInput = readline.question("Enter your name: ")
 console.log(`Hello ${nameInput}!  Welcome to my Island.`)
 
 let monsK1 = readline.question("Enter an animal name: ")
-let monsK1LifeHP = 100
 console.log("\nYou are in vacation island and you ship is waiting for you")
 console.log(`\n${monsK1}! who stolen your wallet, your ticket of the ship is inside`)
 console.log(`\nSmarty pants ${monsK1}! is in top of Plam tree, \nPlease knok it down to retrieve you Wallet`)
 console.log(`\n\n\nThen ${nameInput} start look around and I find some weapons over the island
 `)
 
+let monsK1LifeHP = 100 
 
-while (monsK1LifeHP <= 100){
-if(monsK1LifeHP > 0){
+while (monsK1LifeHP <=100){
+
+if  (monsK1LifeHP > 0){
 var readlineSync = require('readline-sync');
-Weapons = ['Useless Rock', 'Empty Bottle', 'Blue Craby', 'Dead Fish', 'Gaint Rock'],
+Weapons = ['Rock', 'Starfish', 'Stone Crab', 'Dead Shark', 'Blue Lobster'],
 console.log("\n\n\n\n\n```Select from Weapons List below```")
 index = readlineSync.keyInSelect(Weapons, 'Pick your weapons?');
 console.log("\n\n\n\n\nI Pick up a weapon [" + Weapons[index] + `] throw it to ${monsK1}.`);
@@ -36,15 +37,16 @@ while (true) {
   else if (key === 'x') { if (value < MAX) { value++; } }
   else { break; }
 }
+
 value = value * Math.floor(Math.random() * 10)//random att from 1-10
 console.log(`\n${nameInput} try hit with power: ` + value);
-monsK1LifeHP -= value;
-console.log(`\n${monsK1} taking damage ${monsK1LifeHP}`);
-console.log(monsK1LifeHP);
+monsK1LifeHP = monsK1LifeHP - value; //taking count the damage
+console.log(`\n${monsK1} taking damage ${monsK1LifeHP}, try harder if you can!`);
+console.table(`\n${monsK1} still have HP ${monsK1LifeHP}`);
 } 
-else (monsK1LifeHP < 0); {
-console.log(`${nameInput} knok down ${monsK1} badly, \nCongratulations, retrieve your ticket, \nYou can go Home `)
-console.log(monsK1LifeHP);
-break;
+else if(monsK1LifeHP <= 0){
+  console.log(`${nameInput} knok down ${monsK1} badly, \nCongratulations, retrieve your ticket, \nYou can go Home `)
+  break;
 }
+
 }
