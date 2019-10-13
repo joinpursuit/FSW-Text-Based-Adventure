7fb8ab12fe25beaf0360a6c33a1ad4ef0f6dca6c
Jovanni <jovanniluna@pursuit>
2019-10-13 15:02:09 -0400

    delay added function

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -13,55 +13,66 @@ let minotaurHp = 40
 let minotaurAtt = Math.floor(Math.random() * 15);
 let Inventory = 2
 let potionUse = 20
-
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+      end = new Date().getTime();
+   }
+ }
 let nameInput = readline.question("Enter your name: ")
+wait(1000)
 
 console.log(`Hello ${nameInput}!  Welcome to my game.`)
+wait(1000)
 
-
-let classInput = readline.question('Choose a class: Mage or Warrior? ');{
-    if(classInput === "Mage" || classInput === "mage") {
+let classInput = readline.question('Choose a class: Mage or Warrior? '); wait(1000); {
+    if(classInput === "Mage" || classInput === "mage") wait(1000); {
         console.log("Ah, a master of the mystic arts I see, are you ready?")
         let player = {
             Name: nameInput,
             Class: "Mage",
             Health: mageHp
          } //story starting as a Mage
-        console.log(player)
+         wait(1000)
+         console.log(player)
+         wait(1000)
         console.log("-------------------------------------------------")
+        wait(1000)
         console.log("You begin your journey by entering a dark dungeon commanded by your King")
+        wait(1500)
         console.log("He may have mentioned something about slaying a large beast but you weren't paying much attention")
-        let choice1 = readline.question(`You follow a path, however, it spilts into two, do you go left or right? `)
-        console.log("-------------------------------------------------");{
-        if(choice1 === "left" || choice1 === "Left") {
-        console.log("You find an empty room with nothing, maybe everyone left"); // this will lead us to the next room 2 as a mage
-        } else {
+        wait(1500)
+        let choice1 = readline.question(`You follow a path, however, it spilts into two, do you go left or right? `);{
+                if(choice1 === "left" || choice1 === "Left") {
+                    console.log("You find an empty room with nothing, maybe everyone left"); wait(1500);
+                } else { {
             player["Inventory"] = 2
-            console.log("You find a minion inside the room, you two stare at each other and then he lunges at you");
-            console.log("Prepare for battle") //loop coming up for battle update
-            console.log("-------------------------------------------------")
+            console.log("You find a minion inside the room, you two stare at each other and then he lunges at you"); wait(1000);
+            console.log("Prepare for battle"); wait(1000); //loop coming up for battle update
+            console.log("-------------------------------------------------"); wait(1000);
             for(let i = 75; i > -1; i --){
                     i -= minion1Att
-                    console.log("Health: " + mageHp + " Potions: " + Inventory)
+                    console.log("Health: " + mageHp + " Potions: " + Inventory); wait(1000);
                     let battleInput = readline.question(`Do you wish to attack or use a potion? `);{
                         if(battleInput === "attack" || battleInput === "Attack") {
-                            console.log(`You attack for ${mageAtt}`);
-                            console.log(`Minion's health is now ${minion1Hp - mageAtt}`);
+                            console.log(`You attack for ${mageAtt}`); wait(1000);
+                            console.log(`Minion's health is now ${minion1Hp - mageAtt}`); wait(1000);
                             minion1Hp = (minion1Hp - mageAtt)
-                            console.log(`Minion attacks you for ${minion1Att}`);
+                            console.log(`Minion attacks you for ${minion1Att}`); wait(1000);
                             // console.log(`Your health is now at ${mageHp - minion1Att}`); remove for now
-                            console.log("-------------------------------------------------")
+                            console.log("-------------------------------------------------"); wait(1000);
                             mageHp = (mageHp - minion1Att)
                             if (minion1Hp <= 0){
-                                console.log ("You've defeated the minion and exit to the next hallway") //end of battle room loop with only minion health; add your health
+                                console.log ("You've defeated the minion and exit to the next hallway"); wait(1000); //end of battle room loop with only minion health; add your health
                                 break
                             } else if ( mageHp <= 0) {
-                                console.log ("You have been killed! GAME OVER: Try Again")
+                                console.log ("You have been killed! GAME OVER: Try Again"); wait(1000);
                                 process.exit() //completely exit the script like an ending
                         }
                     } else {
                         if( Inventory < 1) {
-                            console.log("Out of potions")
+                            console.log("Out of potions"); wait(1000);
                         } else {
                             mageHp = mageHp + potionUse
                             Inventory = Inventory - 1 // how to get it to stop at 0 potions and repeat: SOLVED!
@@ -70,8 +81,11 @@ let classInput = readline.question('Choose a class: Mage or Warrior? ');{
                 }
             }
         } 
-        console.log ("You find a door on the other side of the room and continue down its hall")
-    }
+    } console.log ("You find a door on the other side of the room and continue down its hall"); wait(1000);
+    console.log("The hall once again, splits into two, game developers aren't that creative anymore"); wait(1000);
+    let choice2 = readline.question(`So you know how this goes, do you go left or right? `); wait(1000);
+    console.log("-------------------------------------------------"); wait(1000);
+}
 }
 }
 //     } else { //story starting as a warrior
