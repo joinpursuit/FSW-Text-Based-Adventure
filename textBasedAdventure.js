a145d579ab6a0ece274e22a8fd2f2ed0708eb19a
Kevin Brutus <kevinbrutus@pursuit.org>
2019-10-17 20:00:56 -0400

    Edits to team choice

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -12,48 +12,31 @@ if(ageInput <= 19){
     console.log(`Hey ${nameInput}, just missed the 90s by the hair of your nose.`)
 } else if (ageInput > 19 && ageInput < 65){
     console.log(`That is a brilliant age ${nameInput}`)
-    console.clear()
 } else if (ageInput >= 65 && ageInput < 110){
     console.log(`Wow ${nameInput}, I see you're of a golden age`)
-    console.clear()
 } else {
     console.log('Does not compute, please try again.')
     ageInput = readline.question(`How old are you ${nameInput}?: `)
 }
 
 
-let teamLight = 'Your weapon is a sword. Your protection is armor.'
-let teamDark = 'Your weapon is a spear. Your protection is a shield.'
 // Team choice
+// let teamLight = 
+// let teamDark = 
+
 let teamChoice = readline.question(`Would you like the path of Light (enter 1) or to walk the Roads of Night (enter 2): `)
-if (teamChoice === 1){
-    console.log(teamLight)
-} else {
-    console.log(teamDark)
+if(teamChoice = 1){
+console.log('Your weapon is a sword. Your protection is armor.')
+} else if (teamChoice = 2){
+console.log('Your weapon is a spear. Your protection is a shield.')
 }
-
-
-
-// let colorInput = readline.question('What color would you like (please only use one): ')
-// if (colorInput === 'blue' || colorInput=== 'red' || colorInput === 'orange' || colorInput === 'yellow' || colorInput === 'white' || colorInput === 'green' || colorInput === 'pink'){
-//     console.log('You are Team Light' + teamLight)
-// } else if (colorInput === 'purple' || colorInput === 'black' || colorInput === 'grey' || colorInput === 'brown' || colorInput === 'gray'){
-//     console.log('You Team Dark' + teamDark)
-// }else {
-//         console.log('Your color taste it too exquisite for our game. Please try again.')
-//         //readline.question('What color would you like (please only use one): ')
-//     }
-
-
-
- let team1 = teamLight
- let team2 = teamDark
- 
-
+    
+let team1 = 1
+let team2 = 2
 if(team1){
      console.log('Welcome to the Light. We must defeat the darkness once and for all to keep balance between the realms')
-     readline.question('What would your first act of peace be: ')
-} else{
+    //  readline.question('What would your first act of peace be: ')
+} else if (team2){
     console.log('Welcome to the Darkness. You are more powerful than our light adversary. To war we go!')
-    readline.question('What would your first act of war be: ')
-}
+    // readline.question('What would your first act of war be: ')
+    }
