2913ebd38c3e7b776cba2b8cb7e9f43bdb92f50f
trilltown <phil.awich@gmail.com>
2019-10-18 17:41:36 -0400

    update

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,18 +1,27 @@
-
 const readline = require('readline-sync')
 
-let intro = ("Welcome to 'Soul Searcher'");
+let intro = ("~ Welcome to 'Tales of Pursuit' ~");
 console.log(intro)
-let dialogue = ("In this adventure you will be embark on a quest to find your missing soul mate");
+console.log()
+let dialogue = ("In this adventure you will be embark on a quest to find yourself and others who will help you along the way. ");
 console.log(dialogue)
+console.log()
+enterGame = readline.question("Press 'enter' to continue:")
+console.log()
+let nameInput = readline.question("What is your name? ")
+console.log()
 
-let enterGame = readline.question("Press 'enter' to continue")
+readline.question(`Hi ${nameInput}! Let us begin with a game of chance. You will now be given a random number. `)
+console.log()
 
-let nameInput = readline.question("Who are you?")
-let hello = readline.question(`Hi ${nameInput}`)
-let Number = readline.question("What's your favorite number?")
-console.log(typeof Number)
+let num1 = Math.floor((Math.random() * 10) * 15);
+readline.question(`Your number is ${num1}. Remember this number. "${num1}"`) 
 
+if(num1 > 50) {
+    confirm("The journey ahead will be very difficult")
 
+    num1 = readline.question("")
+} else if (num < 50) {
+    confirm("You will encounter many friends on your path to ")
+}
 
-// console.log(`Hello ${nameInput}!  Welcome to my Soul Searcher.`)
