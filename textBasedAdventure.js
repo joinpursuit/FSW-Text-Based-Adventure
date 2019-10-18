f1573ac472b11512fe1ea192c768a9b1f199aa90
dannyl <danilovlaurent@pursuit.org>
2019-10-18 01:55:25 -0400

    Game Intro

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,5 +1,38 @@
 const readline = require('readline-sync')
 
-let nameInput = readline.question("Enter your name: ")
+let nameInput = readline.question("Hey! what's your name?: ")
 
-console.log(`Hello ${nameInput}!  Welcome to my game.`)
+console.log(`Hello ${nameInput}!`)
+
+const gameIntro = (space = " ") => {
+
+    let userInput = readline.question("Would you like to play The Dungeon Of Questions?: ")
+console.clear()
+if (userInput === "yes"){
+    console.log(`Welcome to the Dungeon of Questions ${nameInput}!. You must navigate through the dungeon by going through various chambers. Each chamber is gaurded by the Questions  Reeper! Answer the question correctly, continue your path through the dungeon. Answer the question incorrectly, The reeperwill take your soul!, and you     will have to start your journey from the BEGINING Good Luck!`)
+} else {
+    console.log("Aww, Someone's afraid. Well you should be!, Come back at your own risk! ")
+} 
+return space
+}
+console.log(gameIntro());
+
+
+
+
+const startGame = (space = " ") => {
+   let userInput;  
+   while (userInput !== "enter"){
+   userInput = readline.question("Type enter when you are ready to enter the dungeon:")
+}
+console.clear() 
+return space; {
+
+}
+}
+console.log(startGame())
+console.log("Reaper: Stop rite there!");
+
+
+
+
