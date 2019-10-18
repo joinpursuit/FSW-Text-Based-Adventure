8ac2b5ae5e8c0d820898ac06318a0b293fd95733
rafidhossain1995 <rafidhossain@pursuit.org>
2019-10-17 22:16:54 -0400

    project

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -5,7 +5,7 @@ let patient = {
     name: "Jana Nazim",
     age: 23,
     height: "5' 4inches",
-    illness: "FUCKING IDIOT",
+    illness: "None",
     State: "Michigan"
 
 }
@@ -16,15 +16,21 @@ let reaction3 = "I almost thought we were going to lose him, but it looks like t
   let health = 70;
     const takeDamage = (num) =>{
     health -= num
-    }
-    console.log(health)
+    if (health > 0){
+        return health
+        
+    } else if (health <= 0){
+    console.log("The patient has died")
 
+    }
+}
     let health2 = health
     const healthIncrease = (num) => {
     health2 += num
     health2 = health
     }
     console.log(health2)
+
    
 
 
@@ -45,19 +51,19 @@ if (nameInput2 === "yes"){
         console.log("oooh sorry, time is running out")
         takeDamage(10)
         console.log("the patient has " + health + " " + "left")
-        nameInput3 = readline.question("scapel, hammer, or stetescope?")
+        nameInput3 = readline.question("scapel, hammer, or stetescope? ")
     }
 if (nameInput3 === "scapel"){
     console.log(reaction2)
     
-    let nameInput4 = readline.question ("The patient is starting to get weary, should we grab the incubator?")
+    let nameInput4 = readline.question ("The patient is starting to get weary, should we grab the defribullator?")
     while (nameInput4 !== "yes"){
         console.log("What do you mean no? you are wasting time")
         takeDamage(10)
         console.log("the patient has " + health + " life left.")
         nameInput4 = readline.question("yes or no?")
     }
-    if(nameInput4 === "grab the incubator")
+    if(nameInput4 === "grab the incubator" || "yes")
     healthIncrease(10)
     console.log("The patient has " + health2 + " life.")
     console.log(reaction3)
@@ -65,54 +71,68 @@ if (nameInput3 === "scapel"){
     console.log("After giving a few minutes to make sure the body is sedated, the doctor made an incision and opened the chest")
 
     let nameInput5 = readline.question("Doctor, there seems to be a huge black spot in her heart, should we clean that or leave it alone?")
-    if (nameInput5 === "clean"){
+    while(nameInput5 !== "clean"){
+        console.log("Doc, that's only going to create bacteria. We need to clean it.")
+        takeDamage(5)
+        console.log("The patient has " + health + " life left.")
+        nameInput5 = readline.question("clean or leave it alone?")
+    }
+    if (nameInput5 === "clean" || "clean it"){
         console.log("Wow doctor, her health is going up.")
         healthIncrease(20)
-        console.log(health2)
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log(".....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-        console.log("....")
-
         console.log("good job, we kept her alive for a little bit more")
-        let nameInput6 = readline.question("Dr, the operation looks succesful. We can sew up the wounds or wait an hour, What do you want to do?")
-            if (nameInput6 === "sew up the wounds"){
+        
+    }
+    
+        let nameInput6 = readline.question("Dr, the operation looks succesful. We can sew up the wounds or wait an hour, What do you want to do? ")
+            while (nameInput6 !== "sew") {
+            console.log("Doctor, her organs are getting infected. I think you should sew them up")
+            takeDamage (10)
+            console.log("The patient has" + health + "life left. ")
+            nameInput6 = readline.question ("sew up the wound or wait an hour?")
+            }
+            if (nameInput6 === "sew up the wounds" || "sew" || "sew up the wound"){
                 console.log("Ok, looks like we did well")
-                let nameInput7 = readline.question("Doctor you can go home for an hour if you want")
+            }
+                
+            let nameInput7 = readline.question("Doctor you can go home for an hour if you want")
                 while (nameInput7 !== "no"){
                     console.log("DOCTOR HURRY, SHE SEEMS TO BE HAVING ANOTHER HEART ATTACK PLEASE HELP")
                     console.log("DOCTOR???????")
                     console.log("THE PATIENCE HAS DIED")
                     console.log("Let's try to see if we have a better option.")
                     nameInput7 = readline.question("Doctor you can go home if you want")
-                    
-                }   
-            } else if (nameInput7 === "no"){
-                let nameInput7 = readline.question("doctor, the patient had another major attack. He died ,try again?")
-                while (nameInput7 !== "yes") {
-                    console.log("OK what do you want to do now")
+                }
+                
+                 if (nameInput7 === "no"){
+                console.log("The Doctor stayed with the patient for 20 hours while he slept and recovered")
+                 }
+                 
+                 console.log("Ok Doctor, there seems to be a small complication")
+                 console.log("You have to choose between giving him the red pull or blue pill")
+                 let nameInput8 = readline.question("OK doctor, which one is it going to be?")
+                    while (nameInput8 !== "blue"){
+                        console.log("Doctor, the patient is waking up")
+                        console.log("the patient asks where he is and is told about her heart attack.")
+                        console.log("Alright Ms. Nazim, you seem well rested. I think you can go home now.")
+                        console.log("As ms. Nazim steps out of the hospital, she realizes that")
+                        console.log("By taking the red pill, she fell into a rabbits hole and the world is inside a matrix")
+                        console.log("She was too overwhelmed by the truth and couldn't believe what she was seeing")
+                        console.log("Not paying attention, she walked into the road and got hit by a bus and died.")
+                        nameInput8 = readline.question("Ok doctor, is she getting the red or blue pill?")
+                    }
+                    if (nameInput8 === "blue"){
+                        console.log("The patien survived thanks to you doctor.")
+                    console.log("The patient thanks you for her recovery and I hope you have enough time to make it to your pursuit class!")
+                    }
+                
                 }
                 
             }
-         }       
-     }   
+            
+     
+    
 
-}
 
     // create possibilities withion possiblity
     // next create an option that the patient is recovering and needs rest, we can sew her now or wait
