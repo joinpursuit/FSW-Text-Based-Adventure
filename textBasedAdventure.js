1c5032ac088ef9b103f0acf598266c3520755d4a
dannyl <danilovlaurent@pursuit.org>
2019-10-18 23:38:27 -0400

    first part of game created

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -9,9 +9,9 @@ const gameIntro = (space = " ") => {
     let userInput = readline.question("Would you like to play The Dungeon Of Questions?: ")
 console.clear()
 if (userInput === "yes"){
-    console.log(`Welcome to the Dungeon of Questions ${nameInput}!. You must navigate through the dungeon by going through various chambers. Each chamber is gaurded by the Questions  Reeper! Answer the question correctly, continue your path through the dungeon. Answer the question incorrectly, The reeperwill take your soul!, and you     will have to start your journey from the BEGINING Good Luck!`)
+    console.log(`Welcome to the Dungeon of Questions ${nameInput}!. You must navigate through the dungeon by going through various chambers. Each chamber is gaurded by the Questions  Reeper! Answer the question correctly, continue your path through the dungeon. Answer the question incorrectly, The reeper will take your soul!, and you will have to start your journey from the BEGINING Good Luck!`)
 } else {
-    console.log("Aww, Someone's afraid. Well you should be!, Come back at your own risk! ")
+    console.log("Ha!, Someone's afraid. Well you should be!, Come back at your own risk! ")
 } 
 return space
 }
@@ -26,13 +26,112 @@ const startGame = (space = " ") => {
    userInput = readline.question("Type enter when you are ready to enter the dungeon:")
 }
 console.clear() 
-return space; {
 
-}
+return space; 
 }
 console.log(startGame())
-console.log("Reaper: Stop rite there!");
+
+
+function doorSelction(space = "...")  {
+    let userInput;
+    console.log("Reeper: STOP RITE THERE!")
+      userInput = readline.question("Reeper: Which chamber would you like to enter? Type (1) for chamber one or Type (2) for chamber two:");
+    if(userInput === "1"){
+      doorOne();
+    } else {
+      doorTwo();                      
+    
+    }
+    return space;
+    
+}
+    console.log(doorSelction());
 
 
 
 
+function doorOne(space = " ")  {
+    console.clear()
+    let userInput1;
+      console.log("Reeper:I have 300 stickes I gave 199 to Mujtaba. how many do  I have now ?");
+    while (userInput1 !== "c"){
+    if (userInput1 !== "c"){
+      console.log("Reeper: Your answer is incorrect try again or i must take your soul!");
+      userInput1 = readline.question("(A).100 (B).423 (C).101 :PLEASE TYPE THE LETTER THAT CORRESPONDS WITH YOUR ANSWER!:");
+    }
+    }
+    if (userInput1 === "c"){
+      console.log("Reeper: You May Enter")    
+    } 
+    return space;
+}
+
+
+function doorTwo() {
+    console.clear()
+    let userInput1;
+      console.log("Reeper:What is 3 multiplied by 20?");
+    userInput1 = readline.question("if (A). 4 (B). 60 (C). 19 :PLEASE TYPE THE LETTER THAT CORRESPONDS WITH YOUR ANSWER!:");
+    if (userInput1 === "b"){
+      console.log("Reeper: You May Enter")    
+    } else {
+      console.log("Reeper: Try again or i must take your soul!")
+}
+return space
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //  Which chamber would you like to enter? Type (1) for chamber one or Type (2) for chamber two:")),5000);{
+//     if (userInput !== yes){
+//         console.log("Hint: breh")
+//     } else {
+//         console.log("great")            // write a if else statement to enter function of door one or door 2
+//     }
+//     }
+//     return space
+    
+//     }
+//      console.log(doorSelction())
