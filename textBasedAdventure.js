3ccbf410a35c5abaf782c8086ffe5e80e9c2dc2b
Kelvin <kelvinarellano@pursuit.org>
2019-10-18 21:39:57 -0400

    progress

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,6 +1,25 @@
 const readline = require('readline-sync')
 
-let nameInput = readline.question("Enter your name: ")
+const  play = () => {
+    let userInput;
+    userInput = readline.question('Would you like to play?') 
+    while (userInput !== "yes") {
+        console.log("Really?")
+        userInput = readline.question("Would you like to play? Enter yes or no?")
+    } 
+    if (userInput === "yes"); {
+            console.log("You've made the right choice.")
+    userInput = readline.question("Are you having a good day or a bad day?")
+    while (userInput === "good day"||"Good Day"||"Good"||"good") {
+        goodDay()
+    }  else {
+            badDay()
+        }
 
-console.log(`Hello ${nameInput}!  Welcome to my game.`)
+    }
+const badDay = () =>{
 
+}
+const goodDay = () =>{
+
+}
