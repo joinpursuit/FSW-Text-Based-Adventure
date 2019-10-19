dec861e5d958053f00b9a5486d7be3cd8d6401cf
Brandon Brown <brandonb0206@gmail.com>
2019-10-18 22:29:11 -0400

    save changes 2

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -26,44 +26,49 @@ kingdomName()
 
 
 
+console.log("option of who they would like to fight first compared to army strength")
+console.log("WHO WOULD YOU LIKE TO CONQUER FIRST!?")
+
+listRivals()
+
 const fightOption = () => {
     let userInput
-    console.log("option of who they would like to fight first compared to army strength")
-    console.log("Who would you like to conquer first!?")
-    userInput = readline.question("Level options")
+
+    userInput = readline.question("WHO'S KINGDOM WOULD YOU LIKE TO CONQUER!?")
     if (userInput === "John Snow" || userInput === "john snow") {
-        fight1()
+        console.log("YOU HAVE CHOSE TO CHALLENGE " + rivalsName(userInput + "'s KINGDOM, LET THE SHOWDOWN BEGIN !")
+        fight1(myArmySize, jSnowArmy, "John Snow")
     }else if (userInput === "K") || userInput === "k") {
-        fight2()
+        console.log("YOU HAVE CHOSE TO CHALLENGE " + rivalsName(userInput + "'s KINGDOM, LET THE SHOWDOWN BEGIN !")
+        fight1(myArmySize, kArmy, "K")
     }else if (userInput === "B") || userInput === "b") {
-        fight3()
+        console.log("YOU HAVE CHOSE TO CHALLENGE " + rivalsName(userInput + "'s KINGDOM, LET THE SHOWDOWN BEGIN !")
+        fight1(myArmySize, bSnowArmy, "B")
     }
 }
+fightOption()
+
 const rivalsName = (name) => {
     let rName = name
     return rName
 }
 const fight1 = (sizeOfArmy, oppArmy, oppName) => {
-    if(sizeOfArmy > oppArmy) {
-        console.log("CONGRATS!!! YOU HAVE DEFEATED " + oopName + ". NOW CHOOSE WHO YOU WILL CONQUER NEXT !" )
+    if(myWins() === 3){
+        victory()
+    }else if(sizeOfArmy > oppArmy) {
+        listRivals.pop(oppName)
+        sizeOfArmy(oppArmy * .7 + sizeOfArmy)
+        myWins(1)
+        console.log("CONGRATS!!! YOU HAVE DEFEATED " + oopName + ". YOU TOOK 70 PERCENT OF YOUR RIVALS ARMY AND ADDED THEM TO YOUR KINGDOM'S ARMY ! YOU NOW HAVE " + myArmySize + " PEOPLE IN YOUR ARMY ! NOW CHOOSE WHO YOU WILL CONQUER NEXT !" )
     } else {
         console.log("SORRY YOU WERE SADLY DEFEATED TRY AGAIN NEXT TIME")
-        //end program
+        return process.exit()
     }
 //compare strenth to rivals strength
 }
-const fight2 = (sizeOfArmy, oppName) => {
-    //compare strenth to rivals strength
-}
-const fight3 = (sizeOfArmy, oppName) => {
-    //compare strenth to rivals strength
-}
-// const takeDamage = (num) => {
-// //reduce army and save army strength
-
-// }
-const armySize = () => {
+const myArmySize = (num) => {
     let size = 450000
+    size += num
     return size
 }
 const jSnowArmy = () => {
@@ -79,13 +84,18 @@ const kArmy = () => {
     return size
 }
 const listRivals = () => {
-    let arr = ["John Snow", "K", "B"]
+    let arr = ["john snow", "K", "B"]
+    for(let i = 0; i < arr.length; i++){
+        console.log(arr[i])
+    }
+    return arr
 }
-// const armyIncrease = (num) => {
-// // increases army and saves army strength
-// }
-// const inventory = () => {
-// //shows you your updated inventory
-
-// }
+const myWins = (num) => {
+    let counter = 0
+    counter += num
+    return counter
+}
+const victory = () => {
+    console.log("CONGRATS YOU ARE NOW THE KING/QUEEN OVER ALL LANDS WITH AN ARMY SIZE OF " + myArmySize + ". THANKS FOR PLAYING GAME OF THRONES !!")
+}
 
