65a8a64e03be67e938f9248ae073219c7a6640b2
Kelvin <kelvinarellano@pursuit.org>
2019-11-03 07:38:00 -0500

    redo

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,25 +1,43 @@
-const readline = require('readline-sync')
-
-const  play = () => {
+console.log("Sometimes, things just go from bad to worse.")
+const play = () => {
     let userInput;
-    userInput = readline.question('Would you like to play?') 
-    while (userInput !== "yes") {
-        console.log("Really?")
-        userInput = readline.question("Would you like to play? Enter yes or no?")
-    } 
-    if (userInput === "yes"); {
-            console.log("You've made the right choice.")
-    userInput = readline.question("Are you having a good day or a bad day?")
-    while (userInput === "good day"||"Good Day"||"Good"||"good") {
-        goodDay()
-    }  else {
-            badDay()
-        }
+    while(userInput !== "yes") {
+        userInput = readline.question("Want me to tell you about it; yes, or no?")
+    }
+}
+console.log("Well I''d be glad to tell you. Anyone can have a rough time, but let me tell you with my life, lady luck 
+doesn't just ignore me she sent me a restraining order.")
+const dice
+//
+//
+//
+//
+//
+//
+//
+
+//the other day I asked a bell boy to handle my bag and he started feeling up my wife
+//I asked him hey buddy who told you you could mess around with my wife he said everybody
+//people keep saying that dogs and their owners end up looking alike, and when my dog found that out he ran away
+//when i was a kid my parents took me to the zoo an they thanked them for returning me 
+//holloween I open the door and kids give me candy
+
+//developing story and choice of passive or aggresive actions
+const aLikelyStory = () => {
+    let userInput;
+    while (userInput){
 
     }
-const badDay = () =>{
-
 }
-const goodDay = () =>{
+//three parts for initial story
+//1 set up - 
+    //1. loop for type of dog that attacks you
+    //2. loop for number of attacks landed
+//2 conflict
+    //flash array of numbers
+    //assume something significant abour it and return a variable to be used later
+    //lost disoriented and feed up choose a direction to go in north, south,east or west
+//3 resolution
+    //each path leads to a tie in with the number choosen
+    // different ccontext for each direction
 
-}
