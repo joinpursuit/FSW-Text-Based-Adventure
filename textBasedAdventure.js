dcb3c5164a1f6f1e218d4ee14c1a0a796f6268b4
Danielle R Cherry <daniellecherry@pursuit.org>
2019-11-19 15:07:59 -0500

    startLine started

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,19 +1,12 @@
 const readline = require('readline-sync')//DO NOT CHANGE
 
-//could probably use while loopo for the time varible. as the player helps others the time will go down if they run out of time before they make it to the finish line then the loo0p will end and they will be told. >0 play <= 2.5 "show warning half way" 1hr < another warning BUUUUT if you keep updating and showing them in the game play then you wont really need a warning
-// ex while time 
-// could also have a meters or something varible that will be added to their final score
-// Player variables:
-// time
-// GSP
-
 let time = 3.00, GSP = 0;
 let current = (`Current time is: ${time} hrs & current GSP is ${GSP}`);
 
 const play = () => {
     let userInput;
     while (userInput !== "yes") {
-        userInput = readline.question("Are you ready to play the: Good Samaritan Race (yes || no)?! ");
+        userInput = readline.question("Are you ready to play the: Good Samaritan Race (yes || no)?! ").toLowerCase();
     }
     
     console.log("Awesome, lets get started!");
@@ -25,7 +18,7 @@ const play = () => {
     
     console.log(current);
 
-    userInput = readline.question("On your way out the door your mother calls to you. Do you stop and ask what she needs (yes || no)?");
+    userInput = readline.question("On your way out the door your mother calls to you. Do you stop and ask what she needs (yes || no)? ").toLowerCase();
 
 
     if (userInput !== "yes" && userInput !== "no") {
@@ -40,28 +33,47 @@ const play = () => {
 
 const helpMom = () => {
     console.clear();
-    let momHelp = readline.question("Mom: Could you load the diswhaser and take out the trash be fore you head out? This will cost you 30 mins time. Will you help your mom (yes || no)?")
+    
+    let momHelp = readline.question("Mom: Could you load the diswhaser and take out the trash be fore you head out? This will cost you 30 mins time. Will you help your mom (yes || no)? ").toLocaleLowerCase();
+    
     if (momHelp !== "yes" && momHelp !== "no") {
         console.log("You trip and sprang your ankle on the way out the door. :-( try again next time")
     } else if (momHelp === "yes") {
-        console.log("Mom: Thank you so much let me drive you to the race, since you were able to do that for me I'll be on time for work and can drop you on my way!");
-        console.clear();
-        console.log("You only lost 10 mins, youre mother driving you saves you time on yopur commute!");
-        time = 2.50;
-        GSP = 15;
+        console.log("Mom: Thank you so much let me drive you to the race, since you were able to do that for me I'll be on time for work and can drop you on my way!")
+        console.clear()
+        console.log("You only lost 10 mins, youre mother driving you saves you time on yopur commute!")
+        time =- .10;
+        GSP += 15;
     } else if (momHelp === "no") {
-        console.log("Mom: Okay thanks anyway and good luck!");
-        console.log(current)
+        console.log("Mom: Okay thanks anyway and good luck!")
+        console.log(current)//HELP commented out code won't show on console
     }
     enRouteToRace()
 }
 
 const enRouteToRace = () => {
-    
+    console.clear();
+    let helpELderlyMan = readline.question("You have to take the bus and train to get to the start line. The train was delayed costing you 30 mins and the bus will take another 10. You see an older man struggling to get across the street do you go to offer him assistence (yes|| no)?").toLowerCase();
+
+    let timeCheck = current + "If ypu help him it will cost you 10 minutes"
+
+
+    console.log(timeCheck)
+
+    if(helpELderlyMan === "yes")  {
+        time -= .40
+        GPS += 35
+        timeCheck = `${current}`
+        console.log("Elderly man: Thank you so much for you're help. I don't know what I would have done without you!\n")
+    } else if (helpELderlyMan === "no") {
+        console.log(`${current}\n You made it!!!`)
+    }
+    startLine()
 }
 
 const startLine = () => {
-    
+    console.log(`You can take the designated route in front of you (forward) or follow the shady looking shorcut sign in the bushes (right)? `)
+    let forkInTheRoad = readline.question=(`Which will it be (forward || right)? `)
 }
 
 play()
