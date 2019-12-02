343d8a52d7e08788e4b3297f7b963dc0db8135a8
maria liza <mariamartinez@pursuit.org>
2019-12-01 22:30:31 -0500

    reformat tba game

---
+++ practice.js
@@ -0,0 +1,80 @@
+
+// Marializa
+
+//  Martinez
+// Marializa
+//  Martinez
+// Marializa Martinez
+// Marializa
+// Marializa Martinez
+// Marializa Martinez
+// Marializa Martinez
+// Marializa Martinez
+// Marializa Martinez
+// Marializa
+
+
+
+//  Martinez
+// class Cat {
+//     constructor(name) {
+//         this.name = name;
+//         this.toys = ['string', 'ball', 'balloon'];
+
+//     }
+//     meow(toys) {
+//         this.toys = this.toys.every(el => console.log(`${this.name} plays with ${this.toys[0]}`))
+//     }
+// }
+//     let nino = new Cat("nino");
+//     let apollo = new Cat("polo")
+//     console.log(apollo.meow())
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   // TODO: Log the answer in a database
+//   console.log(`Thank you for your valuable feedback: ${answer}`);
+
+//   rl.close();
+// });
+const readlineSync = require("readline-sync")
+
+// readlineSync.setDefaultOptions({prompt: '$ '});
+// readlineSync.setDefaultOptions({
+//     prompt: { // Simple Object that has toString method.
+//       toString: function() {
+//         var rss = process.memoryUsage().rss;
+//         return '[' + (rss > 1024 ? Math.round(rss / 1024) + 'k' : rss) + 'b]$ ';
+//       }
+//     }
+//   });
+
+// Wait for user's response.
+// var userName = readlineSync.question('May I have your name? ');
+// console.log('Hi ' + userName + '!');
+
+// // Handle the secret text (e.g. password).
+// var favFood = readlineSync.question('What is your favorite food? ', {
+//   hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+// });
+// console.log('Oh, ' + userName + ' loves ' + favFood + '!');
+
+// let temperTime = readlineSync.question("...TEMPERING! All we have to do is bring the cocoa butter crystals in perfect alignment by slowly reducing heat and constantly agitating the chocolate, WHILE adding just 1% SEED chocolate to help the crystals get into their best shape, creating beautifully smooth, rich, streak-free chocolate, and we do that by cooling the warm chocolate down to 88.7 F! Could you subtract 88.7 from 108?");
+// arrTemperered = arr => {
+//   arr.map((el, i) => {
+//     let temperIt = "Woohoo! You tempered chocolate! You're a chocolatier! And now, now we're ready to roll!";
+//   if (arr[0] - arr[1] === 19.3) {
+//     temperTime += arr
+//     temperTime = arr[0] - arr[1];
+//   }
+//   return temperIt
+// })
+// }
+// console.log(arrTemperered([108, 88.7]))
+// console.log("Woohoo! You tempered chocolate! You're a chocolatier! And now, now we're ready to roll!");
