19d0b6b20994054dfaf506f4e0dd3e246335edfa
ashya <ashyamanning@pursuit.org>
2019-12-03 01:16:08 -0500

    userInput change

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -21,10 +21,10 @@ console.log(nameInput + " you are a skilled Air Marshall who has served a decade
 
 const search = (ans) => {
     let userInput = ans;
-    if (userInput === "defuse the bomb") {
+    if (userInput === "defuse") {
         console.log("You have no background in Explosive Ordnance Disposal. You set the bomb off and everyone dies!")
         play();
-    } else if (userInput === "call for back-up") {
+    } else if (userInput === "call") {
         console.log("Homeland Security assists on call with instructions to defuse the bomb. It's a success! The bomb is now deactivated! The plane makes an emergency landing and an escort arrives to continue the transport. Being a watchful eye, you notice two unmarked vehicles following the convoy.")
         userInput = readline.question("What do you do?")
     }
@@ -40,7 +40,7 @@ const panic = (something) => {
         let ans = readline.question("Do you...[search] for bomb or get [prisoner]?")
         if (ans === "search") {
             console.log("You locate the bomb in the cargo compartment. You have 30 minutes before detonation.")
-            let answer = readline.question("Do you...defuse the bomb or call for back-up?");
+            let answer = readline.question("Do you...[defuse] the bomb or [call] for back-up?");
             search(answer);
         } else {
             console.log("Prisoner jumps you from the bathroom and manages to steal your firearm.");
@@ -49,7 +49,7 @@ const panic = (something) => {
         }
     } else if (userInput === "prisoner") {
         console.log("Prisoner jumps you from the bathroom and manages to steal your firearm.");
-        userInput = readline.question("Do you...disarm the prisoner or surrender?");
+        userInput = readline.question("Do you...[disarm] the prisoner or [surrender]?");
         fight();
     }
 }
@@ -58,7 +58,7 @@ const fight = (something) => {
     let userInput = something;
     if (userInput === "disarm") {
         console.log("There is a tremendous tussle between the two of you but you are able to secure the prisoner with zipties to his seat.");
-        let answer = readline.question("What do you do next? ease panic or search for bomb");
+        let answer = readline.question("What do you do next? ease panic or [search] for bomb");
         ease(answer);
     } else if (userInput === "surrender") {
         console.log("There is no escape for you. The bomb goes off and everyone dies!")
@@ -76,11 +76,11 @@ const bomb = (something) => {
     let userInput = readline.question("What do you do? [alert] the passengers or [search] for bomb?");
     if (userInput === "alert") {
         console.log("Panic breaks out in the cabin and the plane becomes unstable. In the commotion, your prisoner escapes to the bathroom.")
-        let ans = readline.question("Do you...ease panic or get prisoner?")
+        let ans = readline.question("Do you...ease panic or get [prisoner]?")
         panic(ans);
     } else if (userInput === "search") {
         console.log("You locate the bomb in the cargo compartment. You have 30 minutes before detonation.")
-        let answer = readline.question("Do you...defuse the bomb or call for back-up?")
+        let answer = readline.question("Do you...[defuse] the bomb or [call] for back-up?")
         search(answer);
     }
 }
