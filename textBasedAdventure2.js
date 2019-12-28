ee2c2fa212bfa2f512527b2585c39576274c4786
Kelvin <kelvinarellano@pursuit.org>
2019-12-27 22:47:34 -0500

    initial template for first function done

--- textBasedAdventure2.js
+++ textBasedAdventure2.js
@@ -1,1 +1,10 @@
 const readline =  require("readline-sync");
+const play = () => {
+    console.log("You wake up, feeling a sharp cold wind, and find yourself in the middle of a ragin storm.")
+    let userInput;
+    while(userInput!== "yes"){
+       userInput = readline.question("You wke up after finding")
+    }
+
+}
+play()
