6d7cee0a7dc8f4f0cbc8a7adf747df903d70989c
DejaLFlynn <dejaflynn@pursuit.org>
2019-12-28 11:05:52 -0500

    while loop added

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,5 +1,6 @@
 const readline1 = require('readline-sync')
 
+const play = ()=>{
 let nameInput = readline.question("Enter your name: ")
 console.log(`hello ${nameInput} please play my game `)
 let age = readline.question("How young are you?")
@@ -27,4 +28,41 @@ switch (`${boro}`){
     case "5":
             console.log("Staten Island, New Jersey!")
     break;
+    default:
+        console.log("you're not eligible to buy a baconeggandcheese!")
 }
+let howNy = readline.question("Are you from NYC, not upstate?")
+if(howNy === "no"){
+    console.log("its cool but you cant eat an baconeggandcheese")
+} else if(howNy === "yes"){
+    console.log("you can get a baconeggandcheese, if you dont eat pork, we have vegan options and turkey bacon")
+}
+let firstSurvey = readline.question("how do you say Tremont A or B?")
+if(firstSurvey ==="A"){
+    console.log("Tree-Mott")
+    console.log("Tree-Mont")
+}else if(firstSurvey ==="B"){
+    console.log("Tre'Mont nahhhhh unless you speak French")
+    console.log("you dont know how to say it its cool no one goes there!")
+}
+
+
+}
+const bustelloCafe =()=>{
+    let sugarAmount;
+    sugarAmount = readline.question("How much sugar in your Bustello?")
+    if(sugarAmount === "None"){
+        readline.question("you are from another world, you need a sugar at least")
+    }
+    while(sugarAmount ===3){
+        if(Number(sugarAmount>3)){
+            console.log("check your blood sugar, con tres is the max")
+        } else if(Number(sugarAmount<3)) {
+            console.log("check if you were born on earffff")
+        }else{
+            console.log("youre gucci!")
+        }
+    }
+}
+bustelloCafe()
+play()
