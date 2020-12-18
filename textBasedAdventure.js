c85da5a917e1a88d1c36e766944c37b82dc5de39
cassidybeni <cassidybeni@pursuit.org>
2020-12-18 15:27:24 -0500

    Complete beginGame(), introduces the game to the user

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,5 +1,17 @@
 const readline1 = require('readline-sync')
 
-let nameInput = readline.question("Enter your name: ")
-
-console.log(`Hello ${nameInput}!  Welcome to my game.`)
+const beginGame = () => {
+    console.log("Welcome to my game.")
+    let nameInput = readline1.question("What is your name? \n")
+   // console.log(`Hello ${nameInput}!  Welcome to my game.`)
+    
+    console.log(`Hello ${nameInput}! You're a bank robber and the decisions that you enter in the console determine whether you will go to jail or not, but you don't know which path takes you where and you have chances to escape. Good luck!`)
+    if(readline1.keyInYN("Do you want to play?")){
+        startGame()
+    } else {
+        console.log("Maybe next time!")
+        console.clear()
+        beginGame()
+    }
+}
+beginGame() //This goes at the last line
