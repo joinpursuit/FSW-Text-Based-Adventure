086f528ef94bd50ff629fd3d503c1bca1556b99d
joshuamendez <joshuafmendez@gmail.com>
2020-12-21 13:35:06 -0500

    basic layout start,quit,gameloop functions

--- README.md
+++ README.md
@@ -68,7 +68,7 @@ let yourVariableHere = readline.question("Enter your name: ")
 
 Replace the link below with the link to your Trello project:
 
-[Trello](https://trello.com/b/vbKtMOKj/text-based-adventure-project-plan)
+[Trello](https://trello.com/b/dK8DTkB4/joshua-mendez-project-1-tba)
 
 
 ## Rubric
--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,5 +1,28 @@
-const readline1 = require('readline-sync')
+const readline = require('readline-sync')
 
-let nameInput = readline.question("Enter your name: ")
+//console.clear(); clears the terminal can use when entering new scenario
 
-console.log(`Hello ${nameInput}!  Welcome to my game.`)
+const startGame = () => {
+
+    if(readline.keyInYN('Would you like to play a game?')){
+        console.log('Let\'s start!');
+        gameLoop();
+    }else{
+        console.log('Have a nice life!');
+        quitGame();
+    }
+
+    let nameInput = readline.question("Enter your name: ");
+    console.log(`Hello ${nameInput}!  Welcome to my game.`);
+}
+
+const quitGame = () => {
+    process.exit();
+}
+
+const gameLoop = () =>{
+    console.log('Game started');
+}
+
+startGame();
+
