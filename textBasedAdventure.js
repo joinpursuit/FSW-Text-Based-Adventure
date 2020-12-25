17b1bcd50ecf33317eb91fd937bf5e81359cff63
Marsha Hall <mphall77@gmail.com>
2020-12-25 12:22:56 -0500

    add the bones of the game

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -1,5 +1,62 @@
-const readline1 = require('readline-sync')
+const rls = require("readline-sync");
 
-let nameInput = readline.question("Enter your name: ")
+/// --LEAVE GAME
+const leaveGame = () => {
+  console.log(`Hmmm. ${nameInput}, you're no fun.`);
+  console.log("So long");
+  //console.clear();
+  process.exit();
+};
 
-console.log(`Hello ${nameInput}!  Welcome to my game.`)
+/// --START GAME
+const startGame = () => {
+  console.log("Testing, testing, 1, 2, 3");
+
+};
+
+/// --PLAY
+const play = () => {
+  console.log("OK. Here we go.");
+  if (rls.keyInYN("Ready to play?")) {
+    startGame();
+  } else {
+    leaveGame();
+  }
+};
+
+/// --RULES
+const rules = () => {
+  console.log("Here are the rules:");
+  if (rls.keyInYN("Ready to play?")) {
+    play();
+  } else {
+    leaveGame();
+  }
+};
+
+/// -- INTRO
+let nameInput = rls.question("Enter your name: ");
+console.log(`Hello ${nameInput}!  Welcome to my game.`);
+
+/// --WANT TO PLAY?
+if (rls.keyInYN("Do you want to play a game with me?")) {
+  console.log("Sweet!");
+  rules();
+} else {
+    leaveGame();
+  //console.log(`Hmmm. ${nameInput}, you're no fun.`); 
+  //can also just call leaveGame with a standard message
+}
+
+
+/// --SAMPLE of .trim() and .toLowerCase()
+// while(highOrLow !== "high" && highOrLow !== "low") {
+//     let highOrLow;
+//     highOrLow = rls.question("Was I high or low? \n");
+//     highOrLow =highOrLow.trim().toLowerCase();
+//     // .trim() remove any leading or trailing white space(s)
+//     // .toLowerCase() converts the input to all lowercase letters
+//     if(highOrLow !== "high" && highOrLow !== "low") {
+//         console.log("Not a valid guess. Must be high ot low.")
+//     }
+// }
