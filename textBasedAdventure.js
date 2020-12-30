a38abfc4d08762326e0d153d87f6dc14400c4590
Obaid Rustemi <orustemi@gmail.com>
2020-12-30 15:16:57 -0500

    orbSplitLevel function complete

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -6,7 +6,7 @@ const leaveGame = () => {
   process.exit();
 };
 
-let name =[];
+let name = [];
 
 const greeting = () => {
   console.log("Hello! Welcome to my adventure!");
@@ -29,7 +29,7 @@ const gameIntro = () => {
     "While shopping you encounter a pitch black alleyway with a curious sight\n"
   );
   console.log(
-    "at the very end of the alleyway is a glowing blue orb the size of a car\n"
+    "at the very end of the alleyway is a glowing blue portal the size of a car\n"
   );
 
   if (readLineSync.keyInYN("\n Do you wish to enter the alleyway?")) {
@@ -54,7 +54,7 @@ const startGame = () => {
     "As you approach the orb it silently grows in size and you notice something else..\n"
   );
   console.log(
-    "off to the side and close to the orb is a homeless looking man in a cloak with his head down\n"
+    "off to the side and close to the portal is a homeless looking man in a cloak with his head down\n"
   );
 
   let talkOrJump;
@@ -67,7 +67,7 @@ const startGame = () => {
     );
     talkOrJump = talkOrJump.trim().toLocaleLowerCase();
     if (
-      talkOrJump !== "jump into the portal" &&
+      talkOrJump !== "jump into the portal" ||
       talkOrJump !== "talk to the cloaked man"
     ) {
       console.log("Please enter either..\n");
@@ -83,18 +83,58 @@ const startGame = () => {
 };
 
 const talkToYoda = () => {
-  
   console.log("\n The cloaked man begins to speak in a familiar voice..");
   console.log("\n Look lost you do young human");
   console.log("\n great adventure it is obvious you seek..");
   console.log("\n required your assistance is, a great battle awaits");
   console.log("\n a fabled warrior the Republic seeks..");
-  console.log("\n this person's name has been foretold to me, it is " + name + "!")
-  
-  if (readLineSync.keyInYN("\n do you wish to fullfil your destiny and join the resistance? \n")){
-      orbSplitLevel ();
-    } else {
-        leaveGame();
-    };
+  console.log(
+    "\n this person's name has been foretold to me, it is " + name + "!"
+  );
+
+  if (
+    readLineSync.keyInYN(
+      "\n do you wish to fullfil your destiny and join the resistance? \n"
+    )
+  ) {
+    orbSplitLevel();
+  } else {
+    leaveGame();
+  }
+};
+
+
+
+const orbSplitLevel = () => {
+  console.log("\n May the force be with you..");
+  console.log("\n what was once one portal separates slowly into 3.. ");
+  console.log("\n Yoda: your help each of these 3 worlds requires...");
+  let storySelection = [
+    "Forests of Endor",
+    "Podracing on Tatooine",
+    "The Rescue",
+  ];
+  console.log("Where would you like to assist first?");
+  storySelectionIndex = readLineSync.keyInSelect(storySelection);
+  storySelectionChoice = storySelection[storySelectionIndex];
+  if (storySelectionChoice === "Forests of Endor") {
+    forestsOfEndorLevel();
+  } else if (storySelectionChoice === "Podracing on Tatooine") {
+    podracingOnTatooineLevel();
+  } else if (storySelectionChoice === "The Rescue") {
+    theRescueLevel();
+  } else {
+    leaveGame();
+  }
+};
+
+const forestsOfEndorLevel = () => {
+  console.log("\n Yoda: A wise choice..")
+  console.log("\n Godspeed to you on your journey " + name + "!")
+  console.log("\n and may the Force be with you..")
+
+
 }
+
+
 greeting();
