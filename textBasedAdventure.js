9dc79712003591cb881d9998303c3a72256cd49d
Jenna Qian <jiayin1996@gmail.com>
2020-12-31 09:49:49 -0500

    Thermostat number input

--- textBasedAdventure.js
+++ textBasedAdventure.js
@@ -12,6 +12,42 @@ const exitGame = () => {
 const nameInput = readline.question("Please enter your name: \n")
 const ageInput = readline.question ("Please enter your age: \n")
 
+const newGame = () => {    
+    console.log("\x1b[33m%s\x1b[0m", `Hello, good morning ${nameInput}!~`)
+    console.log("\x1b[33m%s\x1b[0m", "Today is December 25th, Christmas day.")
+    console.log("\x1b[33m%s\x1b[0m", "I have left a present for you last night, it is up to you to find where it is hidden!")
+    console.log("\x1b[33m%s\x1b[0m", "With love, Santa! Ho! Ho! Ho!~\n")
+    let answerPresentHunt = readline.question("Would you like to go on a present hunt? Yes[Y] or No[N]\n")
+    if (answerPresentHunt.toLowerCase() === "yes" || answerPresentHunt.toLowerCase() === "y") {
+        console.log("\nGlad to hear! Which room would you like to check out first?")
+        enterRoom()
+    } else {
+        exitGame ()
+    }
+}
+
+
+const playAgain = () => {
+    let answerPlayAgain = readline.question("Would you like to play again?\n")
+    if (answerPlayAgain.toLowerCase === "yes" || answerPlayAgain.toLowerCase === "y") {
+        newGame()
+    } else {
+        exitGame
+    }
+}
+
+
+const livingRoomTemp = () => {
+    let userInputTemp = readline.question(`${nameInput}. What temperature do you see on thermostat? (Input has to be a number)\n`)
+    if (userInputTemp >= 32) {
+        console.log("The temp is warm enough for you to take a peek at the front porch! You slowly open the front door and CONGRATS!")
+        console.log("The present that Santa left is right infront of your eyes! Good job!~\n")
+        playAgain()
+    } else {
+        console.log("It's too cold outside, even with jacket and gloves, you'll freeze! Staying indoor is a much better option.\n")
+        enterRoom()
+    }
+}
 
 
 const enterRoom = () => {
@@ -19,19 +55,21 @@ const enterRoom = () => {
     let room = ["bedroom", "living room", "kitchen", "basement" ]
     for(let i = 0; i <= room.length - 1; i++){
         if (pickRoom === room[0]) {
-            console.log("You're already in bedroom. Nothing in here.")
+            console.log("You're already in bedroom. Nothing in here.\n")
             break;
         } else if (pickRoom === room[1]){
-            console.log("You walk towards the living room. Infront of the fireplace, there are faint santa boots footprints.")
+            console.log("You walk towards the living room. Infront of the fireplace, there are faint santa footprints.")
+            console.log("Your eyes wander over to the temperature reading on the wall, you walk towards it.\n")
+            livingRoomTemp()
             break;
         } else if (pickRoom === room[2]){
-            console.log("Walking towards the kitchen. The full plate of cookies you left on counter last night is now half emptied.")
+            console.log("Walking towards the kitchen. The full plate of cookies you left on counter last night is now half emptied.\n")
             break;
         } else if (pickRoom === room[3]) {
-            console.log("You open the door that leds to the basement, it's pitch dark, you reach for the light switch. Nothing but spider webs here.")
+            console.log("You open the door that leds to the basement, it's pitch dark, you reach for the light switch. Nothing but spider webs here.\n")
             break;
         } else {
-            console.log ("Please move toward any of these rooms: bedroom, living room, kitchen, or basement to continue the game.")
+            console.log ("Please move toward any of these rooms: bedroom, living room, kitchen, or basement to continue the game.\n")
             break;
         }
     }
@@ -39,19 +77,7 @@ const enterRoom = () => {
 }
 
 
-const newGame = () => {    
-    console.log("\x1b[33m%s\x1b[0m", `Hello, good morning ${nameInput}!~`)
-    console.log("\x1b[33m%s\x1b[0m", "Today is December 25th, Christmas day.")
-    console.log("\x1b[33m%s\x1b[0m", "I have left a present for you last night, it is up to you to find where it is hidden!")
-    console.log("\x1b[33m%s\x1b[0m", "With love, Santa! Ho! Ho! Ho!~\n")
-    let answer = readline.question("Would you like to go on a present hunt? Yes[Y] or No[N]\n")
-    if (answer.toLowerCase() === "yes" || answer.toLowerCase() === "y") {
-        console.log("\nGlad to hear! Which room would you like to check out first?")
-        enterRoom()
-    } else {
-        exitGame ()
-    }
-}
+
 
 if (ageInput <= 12) {
     console.log ("\nSlowly sitting up, you reach for the glass of water on your nightstand. Instead, you feel something else. A letter with your name on it. \n")
