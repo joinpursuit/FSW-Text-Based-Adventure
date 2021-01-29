3fbdde9aa86214457e4973aab32ccfa94bd9f447
Kalilah Clarke <kalilah.clarke@gmail.com>
2021-01-28 19:55:33 -0500

    Still Building

--- MysteryIncorporated.js
+++ MysteryIncorporated.js
@@ -8,6 +8,7 @@ game();
 
 function game() {
   if (readline1.keyInYN("Would you like to play and decide your own fate?")) {
+    console.clear()
     intro();
   } else {
     quitGame();
@@ -20,15 +21,12 @@ function quitGame() {
 }
 
 
-
-
 function intro(){
   console.log(
     "Life is a mystery and things are always changing.\nEvery ACTION has a REACTION so BE CAREFUL it could cost you your LIFE. \nIt's raining and dark outside, the fog clouds your vision as you open the front door. The hairs on the back of your neck begin to stand, something is wrong. What do you do?"
   )
   startGame()
 }
-
 
 function startGame() {
   
@@ -47,12 +45,15 @@ function startGame() {
 
     switch (location) {
       case "A":
+        console.clear()
         toSchool();
         break;
       case "B":
+        console.clear()
         stayHome();
         break;
       case "C":
+        console.clear()
         hangWithFriends();
         break;
       default:
@@ -61,12 +62,11 @@ function startGame() {
   }
 }
 
-
 function toSchool(){
     if (readline1.keyInYN("You open the door and notice that the Hallways are all empty.There's an ominous presence in the air. Would you still like to enter? ")) {
       hallway();
     } else {
-      startGame();
+      resetGame();
     }
   };
   
@@ -76,7 +76,7 @@ function toSchool(){
 //Adventure: Create Variable (Left) = Gym  (Right) = Lunchroom
 // 
 function stayHome() {
-  let answer1 = ["1", "2"];
+   answer1 = ["A", "B"];
   while (answer1 !== null) {
     readline1.setDefaultOptions({
       limit: answer1,
@@ -85,66 +85,116 @@ function stayHome() {
     });
     location = readline1
       .question(
-         "You go back to bed. You being to dose off and all of a sudden you hear the wooden floors begin to creek towards you... Somone is in the house. Do you hide in the 1:closet or under the 2:bed?"
-        ).toUpperCase();
+         "You go back to bed. You being to dose off and all of a sudden you hear the wooden floors begin to creek towards you... Somone is in the house. Do you hide in the A:closet or under the B:bed? "
+        )
+        .toUpperCase()
         switch (location) {
-          case "1":
+          case "A":
             Closet();
             break;
-          case "2":
+          case "B":
             Bed();
             break; 
+            default:
           }
           break;
       }
     }
 
-    function Closet(){}
+    function Closet(){
+      if (readline1.keyInYN("You open the door and notice that the Hallways are all empty. There's an ominous presence in the air. Would you still like to enter? ")) {
+        hallway();
+      } else {
+        resetGame();
+      }
+    }
 
-    function Bed(){}
-  
+    function Bed(){
+      
+    }
   
 // the creeking is getting louder towards your bedroom. Do you hide in the closet or under the bed?
 //Adventure: Create Variable (Closet)= Life  (Bed)= Death
-function hangWithFriends(){
-  let answer1=["A","B"];
-  while(answer1 !== null){
-    readline1.setDefaultOptions({
-      limit:answer1,
-      limitMessage:
-      "HURRY UP BEFORE IT MEANS YOUR LIFE",
-    });
-    let location = readline1
-    .question("You begin walking to the park and you see a whole bunch of people walking towards you. \nDo you walk A:towards them or B: Away from them.")
-    switch (location) {
-      case "A":
-        Toward();
-        break;
-      case "B":
-        Away();
-        break;
-      default:
-    }
-  }
-
   
-
- 
-
-
-
+    function hangWithFriends(){
+      answer1=["A","B"];
+      while(answer1 !== null){
+        readline1.setDefaultOptions({
+          limit:answer1,
+          limitMessage:
+          "HURRY UP BEFORE IT MEANS YOUR LIFE",
+        });
+       let location = readline1
+        .question("You begin walking to the park and you see a whole bunch of people walking towards you. \nDo you walk A: Towards or B: Away From\n")
+        .toUpperCase()
+        switch (location) {
+          case "A":
+            Toward();
+            break;
+            case "B":
+              Away();
+              break;
+             // default:
+              }
+              break;
+            }
+          }
+          
 // // You begin walking to the park and you see a whole bunch of people walking towards you do you walk towards then or away from them.
 // //Adventure: Create Variable (A: Toward)= Death  (B: Away)=Life
-
-
-
-
-// function hallway(){
-  //   You walk down the hallway and hear a scream to your right do you go head to the right and try to be a hero, head left to save your own skin or head back towards the building exit?"
-  // }
-
-    
-    
-    
-    game();
+function Toward(){
+  console.clear()
+  console.log("I'M SORRY WRONG DECISION, AND BECAUSE OF YOUR CHOICE YOU WERE KILLED BY A GROUP OF SOCIOPATHS \nNever walk toward something or someone without having a full understanding of the impact.")
+  resetGame()
+  // create a function that allows the program to reset giving the player the chance to replay 
 }
+
+function Away(){
+    answer1=["A","B"];
+    while(answer1 !== null){
+      readline1.setDefaultOptions({
+        limit:answer1,
+        limitMessage:
+        "HURRY UP BEFORE IT MEANS YOUR LIFE",
+      });
+     location = readline1
+      .question("You decided to turn back you only have two options now \n A:To School B: Back Home.\n")
+      .toUpperCase()
+      switch (location) {
+        case "A":
+          Toward();
+          break;
+          case "B":
+            Away();
+            break;
+           // default:
+       }
+            break;
+      }
+}
+
+
+
+
+function hallway(){
+  //   You walk down the hallway and hear a scream to your right do you go head to the right and try to be a hero, head left to save your own skin or head back towards the building exit?"
+   }
+
+    
+    
+    //Would you like to try again
+  function resetGame(){
+      if (readline1.keyInYN("Would you like to play again?")){
+        console.clear()
+        console.log(`Hello Again ${nameInput}!`)
+        console.log(
+          "Life is a mystery and things are always changing.\nEvery ACTION has a REACTION so BE CAREFUL it could cost you your LIFE. \nIt's raining and dark outside, the fog clouds your vision as you open the front door. The hairs on the back of your neck begin to stand, something is wrong. What do you do?"
+        )
+        startGame();
+      } else {
+        quitGame();
+      }
+    }
+    
+
+// Create hidden path that allows the player to identify certain details within the text to choose correct direction.
