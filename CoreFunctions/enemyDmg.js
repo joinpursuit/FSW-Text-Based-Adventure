5b8247161eabe9e66e5411d71a6f971a4b073ced
Jon Narine <jonnicwolf@gmail.com>
2021-06-26 20:12:21 -0400

    enDmg refactor

--- CoreFunctions/enemyDmg.js
+++ CoreFunctions/enemyDmg.js
@@ -1,17 +1,20 @@
 const dice = require(`./dice.js`)
 
-const enDmg = (enemyAtk, def, enemyLvl) => {
+// const enDmg = (enemyAtk, def, enemyLvl) => {
+const enDmg = (herostat, enemy) => {
     //dice dependencies
     const d6Roll = dice(6, 1)
     const d2Roll = dice(2, 1)
 
+    //character dependencies
+    const heroDef = herostat.def
+    const enemyAtk, enemyLvl;
+    [enemyAtk, enemyLvl] = [enemy.atk, enemy.level]
+
   let damage = Math.ceil(((
-    (2 * enemyLvl + 10) / 150) * (enemyAtk / def) + 2) * d6Roll)
-    if (d2Roll === 2) {
-        let hit = heroStat.health - damage
-        return hit
-    } else if (d2 === 1) {
-        console.log('+ The enemy missed! You take no damage.              +')
-        return 0
-    }
+    (2 * enemyLvl + 10) / 150) * (enemyAtk / heroDef) + 2) * d6Roll)
+
+    d2 === 2
+        ? damage
+        : 0
 }
